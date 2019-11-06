68de96237a091a88befc7db898f4b76c16c32726
Reymark Arsenio <jarvs.tensorbros@gmail.com>
2019-11-06 14:36:21 +0800

    finished UI of coop tab

--- components/tabs/tab1.vue
+++ components/tabs/tab1.vue
@@ -1,12 +1,18 @@
 <template lang="pug">
-  h1 TAB1
+  section.coop-tab
+    p.q-heading Cooperatives near you
+    coops-list
 </template>
 <style lang="scss">
 
 </style>
 <script lang="ts">
 import Vue from 'vue'
+import CoopsList from '../coops-list/coops-list.vue';
 export default Vue.extend({
+  components: {
+    CoopsList,
+  }
   
 })
 </script>
